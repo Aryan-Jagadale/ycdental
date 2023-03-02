546328e42875853da63794576d03f758ba02e79d
Pallavi Langhe <1032202021@mitwpu.edu.in>
2023-03-03 00:33:48 +0530

    footer added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import CarosuelComp from "../components/Carosuel";
 import News from "../components/News";
+import FooterComp from "../components/Footer";
 
 const Home = () => {
   return (
     <>
     <CarosuelComp/>
     <News/>
-    
+    <FooterComp/>
     </>
 
 
