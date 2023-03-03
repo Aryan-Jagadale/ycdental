dd8e468daa490b9dbdaf90742789bdc38c2f634a
Pallavi Langhe <1032202021@mitwpu.edu.in>
2023-03-04 02:21:26 +0530

    welcome component created, footer fixed

--- src/components/HomePage/WelcomeComp.jsx
+++ src/components/HomePage/WelcomeComp.jsx
@@ -1,18 +1,77 @@
 import React from "react";
+import mainProfile from "./../../assets/images/profileMain.svg";
+import "../../style/HomePage/welcomeComp.css";
+import subProfile from "./../../assets/images/subProfilePhoto.svg";
 
 const WelcomeComp = () => {
   return (
-         
-        <div>
-            <div> <center> <header>Welcome to Dental College, Ahmednagar </header>
-            <p>To provide comprehensive primary and specialized oral health care to people, <br/>
-                especially marginalized groups and disseminate knowledge of disease prevention to the public, <br/>
-                and continuing dental education to professionals.</p> </center>
-            </div>
-            <div> </div>
+    <div>
+      <div
+        style={{ fontFamily: "Domine", color: "#032F6E" }}
+        className="flex-1 justify-center content-center text-center my-3 py-3 mx-5"
+      >
+        <h1 className="text-3xl">Welcome to Dental College, Ahmednagar</h1>
+        <p className="text-base">
+          To provide comprehensive primary and specialized oral health care to
+          people, especially marginalized groups and
+        </p>
+        <p className="text-base">
+          disseminate knowledge of disease prevention to the public, and
+          continuing dental education to professionals.
+        </p>
+      </div>
+      <div
+        style={{ fontFamily: "Domine" }}
+        className="profileMain flex flex-col justify-center content-end items-center py-4 text-2xl tracking-wider font-bold"
+      >
+        <h1 className="py-3 text-white">MANAGING COMMITTEE </h1>
+        <img src={mainProfile} alt="" className="w-80 mt-15" />
+        <div className="flex flex-col justify-center text-center">
+          <h1 style={{ color: "#800000" }} className="text-2xl">
+            President
+          </h1>
+          <p style={{ color: "#00205B" }} className="text-base">
+            Hon. Yashwantrao Gadakh
+          </p>
         </div>
-        
-  )
+        <div className="flex flex-row w-screen justify-evenly py-10 px-5">
+          <div className="subProfile w-80 flex flex-col justify-start content-center items-center px-10 py-2">
+            <img src={subProfile} className=" w-40" alt="" srcset="" />
+            <div className="flex flex-col justify-center text-center">
+              <h3 style={{ color: "#800000" }} className="text-sm">
+                President
+              </h3>
+              <p style={{ color: "#00205B" }} className="text-xs">
+                Hon. Yashwantrao Gadakh
+              </p>
+            </div>
+          </div>
+          <div className="subProfile w-80 flex flex-col justify-start content-center items-center px-10 py-2">
+            <img src={subProfile} className=" w-40" alt="" srcset="" />
+            <div className="flex flex-col justify-center text-center">
+              <h3 style={{ color: "#800000" }} className="text-sm">
+                President
+              </h3>
+              <p style={{ color: "#00205B" }} className="text-xs">
+                Hon. Yashwantrao Gadakh
+              </p>
+            </div>
+          </div>
+          <div className="subProfile w-80 flex flex-col justify-start content-center items-center px-10 py-2">
+            <img src={subProfile} className=" w-40" alt="" srcset="" />
+            <div className="flex flex-col justify-center text-center">
+              <h3 style={{ color: "#800000" }} className="text-sm">
+                President
+              </h3>
+              <p style={{ color: "#00205B" }} className="text-xs">
+                Hon. Yashwantrao Gadakh
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default WelcomeComp;
