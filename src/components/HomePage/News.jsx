a8e63a1abe4f15afb8a27613663b348031f290df
Aryan-Jagadale <aryansjagadale@gmail.com>
2023-03-04 18:38:17 +0530

    News Responsive done

--- src/components/HomePage/News.jsx
+++ src/components/HomePage/News.jsx
@@ -52,27 +52,28 @@ const News = () => {
   console.log(isChange);
 
   return (
-    <div className="h-screen bg-[#EFEFEF] py-5 px-10">
+    <div className=" bg-[#EFEFEF] py-5 px-5 sm:px-10">
       <div className="flex items-end w-full py-3">
-        <section className="w-[60%] flex items-center justify-center ">
-          <div className="bg-[#00205B] border-2 border-black w-[15vw] flex items-center justify-center p-2">
+        <section className="w-[100%] flex items-center justify-center md:w-[60%] ">
+          <div className="bg-[#00205B] border-2 border-black w-[50%] flex items-center justify-center p-2 md:w-[15vw]">
             <h3 className="text-lg font-bold uppercase tracking-wide text-[#FFF614]">
               Top News
             </h3>
           </div>
         </section>
 
-        <section className="w-[40%] flex items-center justify-end ">
-          <div className="h-[35px] w-[35px] bg-[#00205B] flex items-center justify-center">
+        <section className="w-[0%] flex items-center justify-end md:w-[40%] ">
+          <div className="h-[35px] w-[35px] bg-[#00205B] items-center justify-center hidden md:flex">
             <img src={Add} alt={"Add button"} />
           </div>
         </section>
       </div>
 
       <div className="">
-        <div className="flex items-center w-full my-3 ">
-          <section className="w-[60%] flex items-center justify-center flex-col gap-3">
-            <div className="small_carousel h-[28vh] w-full my-3 p-5">
+        <div className="flex items-center w-full my-3 gap-2 flex-col-reverse md:flex-row  ">
+          <section className="w-[100%] flex items-center justify-center flex-col m-3 md:w-[60%]">
+
+            <div className="small_carousel h-[28vh] w-full p-0 md:p-5">
 
               <Slider {...settings}>
                 <NewsTopComp />
@@ -81,8 +82,9 @@ const News = () => {
               </Slider>
             </div>
 
-            <div className="auto_scroll">
-              <section className="max-h-[200px] shadow-lg border-2 max-w-[650px] py-2  overflow-hidden">
+
+            <div className="auto_scroll mt-32 sm:mt-[5.5rem] lg:mt-6">
+              <section className="max-h-[300px] shadow-lg border-2 max-w-[650px] py-2  overflow-hidden m-4 sm:max-h-[200px]">
                 <div className="text-center">
                   <h3 className="font-bold text-lg uppercase">Other</h3>
                   <hr className="mb-5" />
@@ -98,13 +100,16 @@ const News = () => {
                 </section>
               </section>
             </div>
+
+
           </section>
 
-          <section className="w-[40%] h-full">
+          <section className="w-[100%] h-full md:w-[40%]">
             <div className="p-5 border border-[#FFD900]">
-              <img src={NewsImage} alt="newsImage" />
+              <img src={NewsImage} alt="newsImage" className="m-0" />
             </div>
           </section>
+
         </div>
       </div>
     </div>
