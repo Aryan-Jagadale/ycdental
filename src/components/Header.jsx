962b762491bb3e71ddc7e5b8a91d98157608a25e
Pranav shelke <88943865+PranavShelke4@users.noreply.github.com>
2023-03-07 10:32:13 +0000

    header and nav bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,25 @@
-import React from 'react';
-
+import React from "react";
+import "../style/HomePage/header.css";
+import Logo from "../assets/images/logo.png";
+import { Row, Col } from "react-bootstrap";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div className="header">
+      <Row className="align-items-center">
+        <Col>
+          <img src={Logo} alt="logo" className="header-img" />
+        </Col>
+        <Col xs={6}>
+        <input className="header-search" placeholder="Search" />
+        <button className="header-btn">Search</button>
+        </Col>
+        <Col>
+        <button className="header-btn2">MUHS Update</button>
+        </Col>
+      </Row>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
