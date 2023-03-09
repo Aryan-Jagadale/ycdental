c843ddae5f3cc3b6cb45e5a45664255a43020a11
Pranav shelke <88943865+PranavShelke4@users.noreply.github.com>
2023-03-09 15:14:38 +0000

    carosuel changes

--- src/components/HomePage/Carosuel.jsx
+++ src/components/HomePage/Carosuel.jsx
@@ -1,29 +1,41 @@
-import { Carousel } from "flowbite-react";
 import React from "react";
+import { Carousel } from "flowbite-react";
 import InnerCaroComp from "./InnerCaroComp";
+import "../../style/HomePage/carosuel.css";
 
 const CarosuelComp = () => {
   return (
-    <div>
-      <div className="lg:h-[120vh] lg:py-2 bg-[#800000] flex items-center justify-center py-0 carosuel">
-        <div className="h-[90%] w-[100%] md:w-[90%] bg-[#800000] shadow-2xl flex justify-center items-center border-1 border-red-100">
-
-        <section className="">
-        <Carousel className="carosuel" slide={false}>
+    <>
+      <div className="carosuel">
+      
+          <Carousel className="carosuel" slide={false}>
             <InnerCaroComp />
 
             <InnerCaroComp />
 
             <InnerCaroComp />
           </Carousel>
-        
-        </section>
-          
-
-
-        </div>
       </div>
-    </div>
+    </>
+
+    // <div>
+    //   <div className="lg:h-[120vh] lg:py-2 bg-[#800000] flex items-center justify-center py-0 carosuel">
+    //     <div className="h-[90%] w-[100%] md:w-[90%] bg-[#800000] shadow-2xl flex justify-center items-center border-1 border-red-100">
+
+    //     <section className="">
+    // <Carousel className="carosuel" slide={false}>
+    //     <InnerCaroComp />
+
+    //     <InnerCaroComp />
+
+    //     <InnerCaroComp />
+    //   </Carousel>
+
+    //     </section>
+
+    //     </div>
+    //   </div>
+    // </div>
   );
 };
 
