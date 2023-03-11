a6469c8063db12a7272a7b4a5a7d5f03660d56e6
Pranav shelke <88943865+PranavShelke4@users.noreply.github.com>
2023-03-11 17:39:56 +0000

    vission mission section

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import AboutCarousel from '../components/AboutPage/AboutCarousel';
 import AboutWelcome from '../components/AboutPage/AboutWelcome';
-import AboutDetails from '../components/AboutPage/AboutDetails'
+import AboutDetails from '../components/AboutPage/AboutDetails';
+import VisionMission from '../components/AboutPage/VisionMission';
 
 const About = () => {
   return (
     <>
     <AboutCarousel/>
     <AboutWelcome/>
+    <VisionMission />
     <AboutDetails/>
     </>
   )
