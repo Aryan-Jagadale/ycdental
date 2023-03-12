79b10c6166640735bfe54efb1c0e7f020537a72e
Aryan-Jagadale <aryansjagadale@gmail.com>
2023-03-12 16:13:44 +0530

    Doneeee

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
         <Route path="/department/oral-medicine-radiology" element={<DepartmentORMEDRAD/>} />
 
       </Routes>
-      {/*<FooterComp/>*/}
+      <FooterComp/>
     </>
   );
 }
