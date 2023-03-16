dcff9492c98fd85d12fa1289890632761663071b
Aryan-Jagadale <aryansjagadale@gmail.com>
2023-03-16 21:43:55 +0530

    MUHS responsivness remain

--- src/utils/MUHS.js
+++ src/utils/MUHS.js
@@ -1,4 +1,4 @@
-{/* 22-2023 pdf */}
+
 import ANNEXURE from "../assets/pdf/2022-23/Annexure.pdf";
 import ANNEXURE2 from "../assets/pdf/2022-23/ANNEXURE2.pdf";
 import ANNEXURE3 from "../assets/pdf/2022-23/ANNEXURE3.pdf";
@@ -17,10 +17,11 @@ import Mandate_Annexure15 from "../assets/pdf/2022-23/Mandate_Annexure15.pdf";
 import Azadi_ka_amrutmohotsav from "../assets/pdf/2022-23/Azadi_ka_amrutmohotsav.pdf";
 import AISHE from "../assets/pdf/2022-23/AISHE.pdf";
 
-{/* 23-2024 pdf */}
+
 import Annexure from "../assets/pdf/23-2024/ANNEXURE1.pdf";
 import Annexure2 from "../assets/pdf/23-2024/2ANNEXURE.pdf";
-import Annexure3 from "../assets/pdf/23-2024/3AANNEXURE.pdf";
+import Annexure3 from "../assets/pdf/23-2024/3ANNEXURE.pdf";
+//import Annexure3 from "../assets/pdf/23-2024/3"
 import Annexure4 from "../assets/pdf/23-2024/4AAnnexure.pdf";
 import Annexure5 from "../assets/pdf/23-2024/5ANNEXURE.pdf";
 import Annexure6 from "../assets/pdf/23-2024/6ANNEXURE.pdf";
@@ -33,161 +34,154 @@ import Annexure12 from "../assets/pdf/23-2024/12ANNEXURE.pdf";
 import Annexure13 from "../assets/pdf/23-2024/13ANNEXURE.pdf";
 import Annexure14 from "../assets/pdf/23-2024/14ANNEXURE.pdf";
 import Annexure15 from "../assets/pdf/23-2024/15Mandate_Annexure.pdf";
-import Azadi_ka_amrutmohotsav from "../assets/pdf/23-2024/Azadi_ka_amrutmohotsav.pdf";
+import Azadi_ka_amrutmohotsav1  from "../assets/pdf/23-2024/Azadi_ka_amrutmohotsav.pdf";
 import SWACCHA_BHARAT_ABHIYAN_PDF from "../assets/pdf/23-2024/SWACCHA_BHARAT_ABHIYAN_PDF.pdf";
 
-
 export const muhs = [
-    {
-        head:"2022-2023",
-        sub:[
-            {
-                "name":"Annexure-I",
-                "pdf":{ANNEXURE}
-            },
-            {
-                "name":"Annexure-II",
-                "pdf":{ANNEXURE2}
-                
-            },
-            {
-                "name":"Annexure-III",
-                "pdf":{ANNEXURE3}
-                
-            },
-            {
-                "name":"Annexure-IV",
-                "pdf":{ANNEXURE4}
-            },
-            {
-                "name":"Annexure-V",
-                "pdf":{ANNEXURE5}
-            },
-            {
-                "name":"Annexure-VI",
-                "pdf":{ANNEXURE6}
-            },
-            {
-                "name":"Annexure-VII",
-                "pdf":{ANNEXURE7}
-            },
-            {
-                "name":"Annexure-VIII",
-                "pdf":{ANNEXURE8}
-            },
-            {
-                "name":"Annexure-IX",
-                "pdf":{ANNEXURE9}
-            },
-            {
-                "name":"Annexure-X",
-                "pdf":{ANNEXUREX}
-            },
-            {
-                "name":"Annexure-XI",
-                "pdf":{ANNEXURE11}
-            },
-            {
-                "name":"Annexure-XII",
-                "pdf":{ANNEXURE12}
-            },
-            {
-                "name":"Annexure-XIII",
-                "pdf":{Mandate_Annexure13}
-            },
-            {
-                "name":"Annexure-XIV",
-                "pdf":{FinalAnnexure14}
-            },
-            {
-                "name":"Annexure-XV",
-                "pdf":{Mandate_Annexure15}
-            },
-            {
-                "name":"Azadi_ka_amrutmohotsav",
-                "pdf":{Azadi_ka_amrutmohotsav}
-            },
-            {
-                "name":"AISHE",
-                "pdf":{AISHE}
-            }
-
-        ]
-            
-        
-
-    },{
-        head:"2023-2024",
-        sub:[
-            {
-                "name":"Annexure-I",
-                "pdf":{Annexure}
-            },
-            {
-                "name":"Annexure-II",
-                "pdf":{Annexure2}
-            },
-            {
-                "name":"Annexure-III",
-                "pdf":{Annexure3}
-            },
-            {
-                "name":"Annexure-IV",
-                "pdf":{Annexure4}
-            },
-            {
-                "name":"Annexure-V",
-                "pdf":{Annexure5}
-            },
-            {
-                "name":"Annexure-VI",
-                "pdf":{Annexure6}
-            },
-            {
-                "name":"Annexure-VII",
-                "pdf":{Annexure7}
-            },
-            {
-                "name":"Annexure-VIII",
-                "pdf":{Annexure8}
-            },
-            {
-                "name":"Annexure-IX",
-                "pdf":{Annexure9}
-            },
-            {
-                "name":"Annexure-X",
-                "pdf":{Annexure10}
-            },
-            {
-                "name":"Annexure-XI",
-                "pdf":{Annexure11}
-            },
-            {
-                "name":"Annexure-XII",
-                "pdf":{Annexure12}
-            },
-            {
-                "name":"Annexure-XIII",
-                "pdf":{Annexure13}
-            },
-            {
-                "name":"Annexure-XIV",
-                "pdf":{Annexure14}
-            },
-            {
-                "name":"Annexure-XV",
-                "pdf":{Annexure15}
-            },
-            {
-                "name":"Azadi_ka_amrutmohotsav",
-                "pdf":{Azadi_ka_amrutmohotsav}
-            },
-            {
-                "name":"SWACCHA_BHARAT_ABHIYAN",
-                "pdf":{SWACCHA_BHARAT_ABHIYAN_PDF}
-            }
-        ]
-
-    }
-]
+  {
+    head: "2022-2023",
+    sub: [
+      {
+        name: "Annexure-I",
+        pdf:  ANNEXURE ,
+      },
+      {
+        name: "Annexure-II",
+        pdf:  ANNEXURE2 ,
+      },
+      {
+        name: "Annexure-III",
+        pdf:  ANNEXURE3 ,
+      },
+      {
+        name: "Annexure-IV",
+        pdf:  ANNEXURE4 ,
+      },
+      {
+        name: "Annexure-V",
+        pdf:  ANNEXURE5 ,
+      },
+      {
+        name: "Annexure-VI",
+        pdf: ANNEXURE6,
+      },
+      {
+        name: "Annexure-VII",
+        pdf: ANNEXURE7,
+      },
+      {
+        name: "Annexure-VIII",
+        pdf: ANNEXURE8,
+      },
+      {
+        name: "Annexure-IX",
+        pdf: ANNEXURE9,
+      },
+      {
+        name: "Annexure-X",
+        pdf: ANNEXUREX,
+      },
+      {
+        name: "Annexure-XI",
+        pdf: ANNEXURE11 ,
+      },
+      {
+        name: "Annexure-XII",
+        pdf: ANNEXURE12 ,
+      },
+      {
+        name: "Annexure-XIII",
+        pdf: Mandate_Annexure13 ,
+      },
+      {
+        name: "Annexure-XIV",
+        pdf: FinalAnnexure14 ,
+      },
+      {
+        name: "Annexure-XV",
+        pdf: Mandate_Annexure15 ,
+      },
+      {
+        name: "Azadi_ka_amrutmohotsav",
+        pdf: Azadi_ka_amrutmohotsav ,
+      },
+      {
+        name: "AISHE",
+        pdf: AISHE ,
+      },
+    ],
+  },
+  {
+    head: "2023-2024",
+    sub: [
+      {
+        name: "Annexure-I",
+        pdf: Annexure ,
+      },
+      {
+        name: "Annexure-II",
+        pdf:  Annexure2 ,
+      },
+      {
+        name: "Annexure-III",
+        pdf:  Annexure3 ,
+      },
+      {
+        name: "Annexure-IV",
+        pdf:  Annexure4 ,
+      },
+      {
+        name: "Annexure-V",
+        pdf:  Annexure5 ,
+      },
+      {
+        name: "Annexure-VI",
+        pdf:  Annexure6 ,
+      },
+      {
+        name: "Annexure-VII",
+        pdf:  Annexure7 ,
+      },
+      {
+        name: "Annexure-VIII",
+        pdf:  Annexure8 ,
+      },
+      {
+        name: "Annexure-IX",
+        pdf:  Annexure9 ,
+      },
+      {
+        name: "Annexure-X",
+        pdf:  Annexure10 ,
+      },
+      {
+        name: "Annexure-XI",
+        pdf:  Annexure11 ,
+      },
+      {
+        name: "Annexure-XII",
+        pdf:  Annexure12 ,
+      },
+      {
+        name: "Annexure-XIII",
+        pdf:  Annexure13 ,
+      },
+      {
+        name: "Annexure-XIV",
+        pdf:  Annexure14 ,
+      },
+      {
+        name: "Annexure-XV",
+        pdf:  Annexure15 ,
+      },
+      {
+        name: "Azadi_ka_amrutmohotsav",
+        pdf:  Azadi_ka_amrutmohotsav1 ,
+      },
+      {
+        name: "SWACCHA_BHARAT_ABHIYAN",
+        pdf:  SWACCHA_BHARAT_ABHIYAN_PDF ,
+      },
+    ],
+  },
+];
