900694f5bc42fa1fd289fad6bd9c6aa3eb0f0b81
Pallavi Langhe <1032202021@mitwpu.edu.in>
2023-03-16 14:38:19 +0530

    mhus update

--- src/utils/MUHS.js
+++ src/utils/MUHS.js
@@ -1,5 +1,39 @@
-import Annexure1 from "../assets/pdf/2022-23/Annexure.pdf";
-import Annexure from "../assets/pdf/23-2023/ANNEXURE1.pdf";
+import ANNEXURE from "../assets/pdf/2022-23/Annexure.pdf";
+import ANNEXURE2 from "../assets/pdf/2022-23/ANNEXURE2.pdf";
+import ANNEXURE3 from "../assets/pdf/2022-23/ANNEXURE3.pdf";
+import ANNEXURE4 from "../assets/pdf/2022-23/Annexure4.pdf";
+import ANNEXURE5 from "../assets/pdf/2022-23/Annexure5.pdf";
+import ANNEXURE6 from "../assets/pdf/2022-23/Annexure6.pdf";
+import ANNEXURE7 from "../assets/pdf/2022-23/Annexure7.pdf";
+import ANNEXURE8 from "../assets/pdf/2022-23/Annexure8.pdf";
+import ANNEXURE9 from "../assets/pdf/2022-23/ANNEXURE9.pdf";
+import ANNEXUREX from "../assets/pdf/2022-23/AnnexureX.pdf";
+import ANNEXURE11 from "../assets/pdf/2022-23/ANNEXURE11.pdf";
+import ANNEXURE12 from "../assets/pdf/2022-23/ANNEXURE12.pdf";
+import Mandate_Annexure13 from "../assets/pdf/2022-23/Mandate_Annexure13.pdf";
+import FinalAnnexure14 from "../assets/pdf/2022-23/FinalAnnexure14.pdf";
+import Mandate_Annexure15 from "../assets/pdf/2022-23/Mandate_Annexure15.pdf";
+import Azadi_ka_amrutmohotsav from "../assets/pdf/2022-23/Azadi_ka_amrutmohotsav.pdf";
+import AISHE from "../assets/pdf/2022-23/AISHE.pdf";
+
+import Annexure from "../assets/pdf/23-2024/ANNEXURE1.pdf";
+import Annexure2 from "../assets/pdf/23-2024/2ANNEXURE.pdf";
+import Annexure3 from "../assets/pdf/23-2024/3AANNEXURE.pdf";
+import Annexure4 from "../assets/pdf/23-2024/4AAnnexure.pdf";
+import Annexure5 from "../assets/pdf/23-2024/5ANNEXURE.pdf";
+import Annexure6 from "../assets/pdf/23-2024/6ANNEXURE.pdf";
+import Annexure7 from "../assets/pdf/23-2024/7ANNEXURE.pdf";
+import Annexure8 from "../assets/pdf/23-2024/8Annexure.pdf";
+import Annexure9 from "../assets/pdf/23-2024/9AAnnexure.pdf";
+import Annexure10 from "../assets/pdf/23-2024/10Annexure.pdf";
+import Annexure11 from "../assets/pdf/23-2024/11ANNEXURE.pdf";
+import Annexure12 from "../assets/pdf/23-2024/12ANNEXURE.pdf";
+import Annexure13 from "../assets/pdf/23-2024/13ANNEXURE.pdf";
+import Annexure14 from "../assets/pdf/23-2024/14ANNEXURE.pdf";
+import Annexure15 from "../assets/pdf/23-2024/15Mandate_Annexure.pdf";
+import Azadi_ka_amrutmohotsav from "../assets/pdf/23-2024/Azadi_ka_amrutmohotsav.pdf";
+import SWACCHA_BHARAT_ABHIYAN_PDF from "../assets/pdf/23-2024/SWACCHA_BHARAT_ABHIYAN_PDF.pdf";
+
 
 export const muhs = [
     {
@@ -7,11 +41,75 @@ export const muhs = [
         sub:[
             {
                 "name":"Annexure-I",
-                "pdf":{Annexure1}
+                "pdf":{ANNEXURE}
             },
             {
+                "name":"Annexure-II",
+                "pdf":{ANNEXURE2}
                 
+            },
+            {
+                "name":"Annexure-III",
+                "pdf":{ANNEXURE3}
+                
+            },
+            {
+                "name":"Annexure-IV",
+                "pdf":{ANNEXURE4}
+            },
+            {
+                "name":"Annexure-V",
+                "pdf":{ANNEXURE5}
+            },
+            {
+                "name":"Annexure-VI",
+                "pdf":{ANNEXURE6}
+            },
+            {
+                "name":"Annexure-VII",
+                "pdf":{ANNEXURE7}
+            },
+            {
+                "name":"Annexure-VIII",
+                "pdf":{ANNEXURE8}
+            },
+            {
+                "name":"Annexure-IX",
+                "pdf":{ANNEXURE9}
+            },
+            {
+                "name":"Annexure-X",
+                "pdf":{ANNEXUREX}
+            },
+            {
+                "name":"Annexure-XI",
+                "pdf":{ANNEXURE11}
+            },
+            {
+                "name":"Annexure-XII",
+                "pdf":{ANNEXURE12}
+            },
+            {
+                "name":"Annexure-XIII",
+                "pdf":{Mandate_Annexure13}
+            },
+            {
+                "name":"Annexure-XIV",
+                "pdf":{FinalAnnexure14}
+            },
+            {
+                "name":"Annexure-XV",
+                "pdf":{Mandate_Annexure15}
+            },
+            {
+                "name":"Azadi_ka_amrutmohotsav",
+                "pdf":{Azadi_ka_amrutmohotsav}
+            },
+            {
+                "name":"AISHE",
+                "pdf":{AISHE}
             }
+
         ]
             
         
@@ -22,6 +120,70 @@ export const muhs = [
             {
                 "name":"Annexure-I",
                 "pdf":{Annexure}
+            },
+            {
+                "name":"Annexure-II",
+                "pdf":{Annexure2}
+            },
+            {
+                "name":"Annexure-III",
+                "pdf":{Annexure3}
+            },
+            {
+                "name":"Annexure-IV",
+                "pdf":{Annexure4}
+            },
+            {
+                "name":"Annexure-V",
+                "pdf":{Annexure5}
+            },
+            {
+                "name":"Annexure-VI",
+                "pdf":{Annexure6}
+            },
+            {
+                "name":"Annexure-VII",
+                "pdf":{Annexure7}
+            },
+            {
+                "name":"Annexure-VIII",
+                "pdf":{Annexure8}
+            },
+            {
+                "name":"Annexure-IX",
+                "pdf":{Annexure9}
+            },
+            {
+                "name":"Annexure-X",
+                "pdf":{Annexure10}
+            },
+            {
+                "name":"Annexure-XI",
+                "pdf":{Annexure11}
+            },
+            {
+                "name":"Annexure-XII",
+                "pdf":{Annexure12}
+            },
+            {
+                "name":"Annexure-XIII",
+                "pdf":{Annexure13}
+            },
+            {
+                "name":"Annexure-XIV",
+                "pdf":{Annexure14}
+            },
+            {
+                "name":"Annexure-XV",
+                "pdf":{Annexure15}
+            },
+            {
+                "name":"Azadi_ka_amrutmohotsav",
+                "pdf":{Azadi_ka_amrutmohotsav}
+            },
+            {
+                "name":"SWACCHA_BHARAT_ABHIYAN",
+                "pdf":{SWACCHA_BHARAT_ABHIYAN_PDF}
             }
         ]
 
