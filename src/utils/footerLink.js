9d1d0b40d6bcbcea9385b9cf00ade0c7402fc6b0
Aryan-Jagadale <aryansjagadale@gmail.com>
2023-03-19 12:47:53 +0530

    Done footer

---
+++ src/utils/footerLink.js
@@ -0,0 +1,25 @@
+export const footerLink = [
+    {
+        "name":"MDS Admission",
+        "urll":"/"
+    },
+    {
+        "name":"BDS Admission",
+        "urll":"/"
+    },
+
+    {
+        "name":"Prospectus",
+        "urll":"/"
+    },
+    {
+        "name":"Social Media",
+        "urll":"/"
+    },
+    {
+        "name":"Work with us",
+        "urll":"/"
+    },
+
+
+]
