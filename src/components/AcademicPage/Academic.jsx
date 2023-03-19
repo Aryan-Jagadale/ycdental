6993040a7da0f4b667ff91f5d1ef304354e25f09
Aryan-Jagadale <aryansjagadale@gmail.com>
2023-03-19 14:02:12 +0530

    Banner done

--- src/components/AcademicPage/Academic.jsx
+++ src/components/AcademicPage/Academic.jsx
@@ -5,14 +5,14 @@ const Academic = () => {
   return (
     <div>
 
-    <section className="bg-[#800000] w-full">
-        <div className="flex items-center justify-center w-full p-5">
+    <section className="acdemicsBg">
+        <div className="flex items-center justify-center w-full p-0">
           <div className="w-full md:w-1/2 m-3 md:m-5">
             <div className="w-full border-l-2 border-[#FFD900]">
             
 
               <section className="px-6 flex items-center gap-3 mb-4">
-                <h1 className="text-5xl font-bold font-fam text-white tracking-wide pl-12">
+                <h1 className="text-5xl   font-bold font-fam text-white tracking-wide pl-12">
                   Our
                   <br />
                   Academics
@@ -29,7 +29,7 @@ const Academic = () => {
         </div>
       </section>
 
-      
+
     
     
     
