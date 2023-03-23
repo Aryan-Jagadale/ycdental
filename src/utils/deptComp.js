6eca44e7ee326747cf452617ab73925b03b061fd
Pallavi Langhe <1032202021@mitwpu.edu.in>
2023-03-23 23:17:51 +0530

    deptcomp updated

--- src/utils/deptComp.js
+++ src/utils/deptComp.js
@@ -24,3 +24,279 @@ export const omrdLink = [
     position: "Tutor",
   },
 ];
+
+export const periodontologyLink = [
+  {
+    name: "Dr. Rajhans Nilima",
+    position: "Dean, Professor & HOD",
+  },
+  {
+    name: "Dr. Moolya Nikesh",
+    position: "Prof.",
+  },
+  {
+    name: "Dr. Landge Nilima",
+    position: "Prof",
+  },
+  {
+    name: "Dr. Ajwani Himanshu",
+    position: "Reader",
+  },
+  {
+    name: "Dr. Shinde Sagar",
+    position: "Reader",
+  },
+  {
+    name: "Dr. Sachin Funde",
+    position: "Lecturer",
+  },
+  {
+    name: "Lawande Asawari",
+    position: "Lecturer",
+  },
+  {
+    name: "Dr. Jadhav Swapnil",
+    position: "Lecturer",
+  },
+  {
+    name: "Dr. Berad Prajakta",
+    position: "Tutor",
+  },
+  {
+    name: "Dr. Kadam Bharat",
+    position: "Tutor",
+  },
+  {
+    name: "Dr. Sale Sachin",
+    position: "Tutor",
+  },
+];
+
+export const conservativeDentistryLink = [
+  {
+    name: "Dr. Rawtiya Manjusha ",
+    position: "Prof. & HOD",
+  },
+  {
+    name: "Dr. Mahajan Vishal",
+    position: "Prof.",
+  },
+  {
+    name: "Dr. Gaikwad Bhavana",
+    position: "Prof",
+  },
+  {
+    name: "Dr. Dhoot Rohit",
+    position: "Reader",
+  },
+  {
+    name: "Dr. Daule Rajendra",
+    position: "Reader",
+  },
+  {
+    name: "Dr. Patil Sandip",
+    position: "Reader",
+  },
+  {
+    name: "Dr. Mahajan Rohini Prakash ",
+    position: "Reader",
+  },
+  {
+    name: "Dr.Walkar Kunal",
+    position: "Lecturer",
+  },
+  {
+    name: "Dr. Kothari Pawan",
+    position: "Lecturer",
+  },
+  {
+    name: "Dr. Lonare Saurabh",
+    position: "Lecturer",
+  },
+  {
+    name: "Dr. Palwe Snehal",
+    position: "Lecturer",
+  },
+  {
+    name: "Dr. Yegnul Sayali",
+    position: "Lecturer",
+  },
+  {
+    name: "Dr. Pranali Satpute",
+    position: "Lecturer",
+  },
+  {
+    name: "Dr. Rasik Bagade",
+    position: "Lecturer",
+  },
+  {
+    name: "Dr. Jha Sonikumari",
+    position: "Lecturer",
+  },
+  {
+    name: "Dr. Newase Pooja Tulashiram ",
+    position: "Lecturer",
+  },
+  {
+    name: "Dr. Dhekane Akshada",
+    position: "Tutor",
+  },
+  {
+    name: "Dr. D'Silva Sanju",
+    position: "Tutor",
+  },
+];
+
+export const prosthodonticsLink = [
+  {
+    name: "Dr. Amrita Pandit ",
+    position: "Prof. & HOD",
+  },
+  {
+    name: "Dr. V. N. V. Madhav",
+    position: "Prof.",
+  },
+  {
+    name: "Dr. Aruna Bhandari",
+    position: "Prof.",
+  },
+  {
+    name: "Dr. Kulkarni Manisha",
+    position: "Reader",
+  },
+  {
+    name: "Dr. Karande Nilima",
+    position: "Reader",
+  },
+  {
+    name: "Dr. Manasi Oswal",
+    position: "Reader",
+  },
+  {
+    name: "Dr. Raikar Sonal",
+    position: "Reader",
+  },
+  {
+    name: "Dr. Nalawade Sumit",
+    position: "Lecturer",
+  },
+  {
+    name: "Dr. Attal Pratik",
+    position: "Lecturer",
+  },
+  {
+    name: "Dr. Ugale Vishakha",
+    position: "Lecturer",
+  },
+  {
+    name: "Dr. Omkar Sakpal",
+    position: "Lecturer",
+  },
+  {
+    name: "Dr. Pratik Hodar",
+    position: "Lecturer",
+  },
+  {
+    name: "Dr. Mohammad Wasimuddin",
+    position: "Lecturer",
+  },
+  {
+    name: "Dr. Motadu Prathamesh",
+    position: "Lecturer",
+  },
+  {
+    name: "Dr. Kandi Nikita",
+    position: "Lecturer",
+  },
+  {
+    name: "Dr. Wadkar Saili",
+    position: "Lecturer",
+  },
+  {
+    name: "Dr. Gorde Krishna",
+    position: "Lecturer",
+  },
+  {
+    name: "Dr Pillewar Anjusha",
+    position: "Lecturer",
+  },
+  {
+    name: "Dr. Tellawar Shilpa",
+    position: "Tutor",
+  },
+  {
+    name: "Dr. Jaggi Dimpal",
+    position: "Tutor",
+  },
+  {
+    name: "Dr. Gupta Pooja",
+    position: "Tutor",
+  },
+  {
+    name: "Dr. Kavita Shirsath",
+    position: "Tutor",
+  },
+];
+
+export const orthodonticsLink = [
+  {
+    name: "Dr. Patil Tushar",
+    position: "Prof. & HOD",
+  },
+  {
+    name: "Dr. Mahamuni Avinash",
+    position: "Prof.",
+  },
+  {
+    name: "Dr. Misal Abhijit",
+    position: "Reader",
+  },
+  {
+    name: "Dr. Nawale Priyanka",
+    position: "Reader",
+  },
+  {
+    name: "Dr. Rai Rajlaxmi",
+    position: "Reader",
+  },
+  {
+    name: "Dr. Pawar Vaibhav",
+    position: "Lecturer",
+  },
+  {
+    name: "Dr. Pathak Snehal",
+    position: "Lecturer",
+  },
+  {
+    name: "Dr. Reyali Gajare",
+    position: "Lecturer",
+  },
+  {
+    name: "Dr. Kanchan Purswani",
+    position: "Lecturer",
+  },
+  {
+    name: "Dr. Lokhande Rajkiran",
+    position: "Lecturer",
+  },
+  {
+    name: "Dr. Shivale Shrikant",
+    position: "Lecturer",
+  },
+  {
+    name: "Dr. Harsha Mahajan",
+    position: "Lecturer",
+  },
+  {
+    name: "Dr. Rajput Raksha",
+    position: "Tutor",
+  },
+  {
+    name: "Dr. Gosavi Rohini",
+    position: "Tutor",
+  },
+  {
+    name: "Dr. Chavan Mayuri",
+    position: "Tutor",
+  },
+];
