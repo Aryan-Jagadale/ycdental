871f0ca51438ad002762b4aa8ac3e654cc5dcce2
Aryan-Jagadale <aryansjagadale@gmail.com>
2023-03-23 11:13:29 +0530

    Prospectus pdf

--- src/components/AdmissionPage/Prospectus.jsx
+++ src/components/AdmissionPage/Prospectus.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import CollegeProspectus from "../../assets/pdf/College-Prospectus.pdf";
+import pdfImg from "../../assets/images/muhs/pdfImg.png";
 
 const Prospectus = () => {
   return (
-    <div>Prospectus</div>
-  )
-}
+    <div className="m-5 border-2 border-black p-4">
+      <section className="font-fam flex items-center justify-center gap-3 flex-col">
+        <h2 className="text-3xl tracking-wide font-bold ">Prospectus</h2>
+        <button className="bg-[#FFD900]  flex items-center justify-center p-2 md:p-3">
+          <img src={pdfImg} alt="PDF" className="mr-2" />
+          <span className="tracking-wide text-blue-500">
+            <a href={CollegeProspectus} target="_blank" rel="noreferrer">
+              Download PDF
+            </a>
+          </span>
+        </button>
+      </section>
+    </div>
+  );
+};
 
-export default Prospectus
+export default Prospectus;
