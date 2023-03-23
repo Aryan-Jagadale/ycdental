dad6a162b12012958675ce28779f503df1da93f7
Aryan-Jagadale <aryansjagadale@gmail.com>
2023-03-23 18:54:13 +0530

    Home and Responsive done

--- src/components/HomePage/Carosuel.jsx
+++ src/components/HomePage/Carosuel.jsx
@@ -30,24 +30,7 @@ const CarosuelComp = () => {
       </div>
     </>
 
-    // <div>
-    //   <div className="lg:h-[120vh] lg:py-2 bg-[#800000] flex items-center justify-center py-0 carosuel">
-    //     <div className="h-[90%] w-[100%] md:w-[90%] bg-[#800000] shadow-2xl flex justify-center items-center border-1 border-red-100">
 
-    //     <section className="">
-    // <Carousel className="carosuel" slide={false}>
-    //     <InnerCaroComp />
-
-    //     <InnerCaroComp />
-
-    //     <InnerCaroComp />
-    //   </Carousel>
-
-    //     </section>
-
-    //     </div>
-    //   </div>
-    // </div>
   );
 };
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,32 +3,6 @@ import CarosuelComp from "../components/HomePage/Carosuel";
 import Welcome from "../components/HomePage/Welcome";
 import News from "../components/HomePage/News";
 import Recognition from "../components/HomePage/Recognition";
-//import AboutComp from "../components/HomePage/AboutComp";
-//import Loadable from "react-loadable";
-//import Loader from "../components/Loader/index"
-
-/*const loader = () => <Loader/>*/
-
-/*const CarouselLazy = Loadable({
-  loader: () => <CarosuelComp/>,
-  loading: loader,
-});
-
-
-const WelComeLazy = Loadable({
-  loader: () => <Welcome/>,
-  loading: loader,
-});
-
-const AboutLazy = Loadable({
-  loader: () => <AboutComp/>,
-  loading: loader,
-});
-
-const NewsLazy = Loadable({
-  loader: () => <News/>,
-  loading: loader,
-});*/
 
 const Home = () => {
   return (
