fcac9de0c8a33d53c561d1fddb96319e179d2086
Pallavi Langhe <1032202021@mitwpu.edu.in>
2023-03-24 00:15:50 +0530

    dept update

--- src/utils/deptComp.js
+++ src/utils/deptComp.js
@@ -538,31 +538,3 @@ export const generalMedicineLink = [
     position: "Lecturer",
   },
 ];
-export const generalSurgeryLink = [
-  {
-    name: "Dr. Jadhav Bhaskar",
-    position: "Reader & HOD",
-  },
-  {
-    name: "Dr. Nagargoje Vaishali",
-    position: "Lecturer",
-  },
-  {
-    name: "Dr. Wakpanjar Jayshri",
-    position: "Lecturer",
-  },
-  {
-    name: "Dr.Shirsat Sagar",
-    position: "Lecturer",
-  },
-];
-export const anesthesiaLink = [
-  {
-    name: "Dr. Rethe Prakash",
-    position: "Reader & HOD",
-  },
-  {
-    name: "Dr. Kadam Mahesh",
-    position: "Lecturer",
-  },
-];
