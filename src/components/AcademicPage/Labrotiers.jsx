dea9d57a26471cc3717572d7acfa408871ecfab1
Aryan-Jagadale <aryansjagadale@gmail.com>
2023-03-24 19:47:21 +0530

    Done Lab and ug pg images

--- src/components/AcademicPage/Labrotiers.jsx
+++ src/components/AcademicPage/Labrotiers.jsx
@@ -13,19 +13,19 @@ const Labrotiers = () => {
         <div>
           <section className="grid-4 flex items-center justify-around gap-3 mt-4">
             {lab4.map((item) => (
-              <GridLab4 key={item} item={item} />
+              <GridLab4 key={item.label} item={item} />
             ))}
           </section>
 
           <section className="grid-4 flex items-center justify-around  gap-3 mt-3">
             {lab3.map((item) => (
-              <GridLab3 item={item} />
+              <GridLab3 key={item.label} item={item} />
             ))}
           </section>
 
           <section className="grid-4 flex items-center justify-around gap-3  mt-3">
             {lab41.map((item) => (
-              <GridLab4 key={item} item={item} />
+              <GridLab4 key={item.label} item={item} />
             ))}
           </section>
         </div>
