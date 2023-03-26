feae13a0efa7ecb353d220cbe93458172b19d350
Pranav shelke <88943865+PranavShelke4@users.noreply.github.com>
2023-03-26 13:14:51 +0000

    .

--- src/utils/GalleryData.js
+++ src/utils/GalleryData.js
@@ -1,24 +1,23 @@
 const GalleryData = [
   {
     id: 1,
-    days: "Dentist Day",
+    days: "Ambedkar Jayanti",
     image: [
-        "https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8&w=1000&q=80",
-        "https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8&w=1000&q=80",
-        "https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8&w=1000&q=80",
-        "https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8&w=1000&q=80",
+      "https://www.yashdental.org/wp-content/uploads/2021/01/2-350x250.jpg",
+      "https://www.yashdental.org/wp-content/uploads/2021/01/3-1-350x250.jpg",
+      "https://www.yashdental.org/wp-content/uploads/2021/01/1-350x250.jpg",
     ],
     date: "6th",
     month: "March",
     description:
-      "Every year March 6th is observed as National Dentist Day, created to say thank you to all dentists celebrating for their contribution.There are various dental specialities and on this special occasion, it is better to know and recognise them. Dentists are often referred to as dental surgeons or family dentist. They are specialised in treating oral health; it is a comprehensive integrated approach in a field to overall health.",
+      "The entire nation knows about the legendary “Bhimrao Ramji Ambedkar”. He was one of the greatest leaders of India who was more of a social worker, a writer, and an educationist. He always worked for the Social Justice of untouchables and was the one who drafted Indian Constitution. He was like an angel for the poor and low caste people, this is a reason people still worship him like a God.",
   },
   {
     id: 2,
     days: "Dentist Day",
     image: [
-        "https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8&w=1000&q=80",
-        "https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8&w=1000&q=80",
+      "https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8&w=1000&q=80",
+      "https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8&w=1000&q=80",
     ],
     date: "6th",
     month: "March",
@@ -27,16 +26,38 @@ const GalleryData = [
   },
   {
     id: 3,
-    days: "Dentist Day",
+    days: "What After BDS - Rajeev Chitgoppi",
     image: [
-        "https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8&w=1000&q=80",
-        "https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8&w=1000&q=80",
+      "https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8&w=1000&q=80",
+      "https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8&w=1000&q=80",
     ],
     date: "6th",
     month: "March",
     description:
       "Every year March 6th is observed as National Dentist Day, created to say thank you to all dentists celebrating for their contribution.There are various dental specialities and on this special occasion, it is better to know and recognise them. Dentists are often referred to as dental surgeons or family dentist. They are specialised in treating oral health; it is a comprehensive integrated approach in a field to overall health.",
   },
+  {
+    id: 4,
+    days: "International Women Day",
+    image: [
+      "https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8&w=1000&q=80",
+      "https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8&w=1000&q=80",
+    ],
+    date: "6th",
+    month: "March",
+    description:
+      "An equal world is an enabled world. How will you help forge a gender equal world? Celebrate women's achievement. Raise awareness against bias. Take action for equality.International Women's Day (IWD) is celebrated on the 8th of March every year around the world. It is a focal point in the movement for women's rights." },
+  {
+    id: 5,
+    days: "Shiv Jayanti",
+    image: [
+      "https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8&w=1000&q=80",
+      "https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8&w=1000&q=80",
+    ],
+    date: "6th",
+    month: "March",
+    description:
+     "Shiv Jayanti or Chhatrapati Shivaji Maharaj Jayanti is the birth anniversary of Maratha king Shivaji. Shiv Jayanti is celebrated on February 19 every year in his honour.The creator and founder of the Maratha Empire, Chhatrapati Shivaji Maharaj was born in 1630 at Shivneri Fort near Pune.He was named after a regional Goddess Shivai. Chhatrapati Shivaji Maharaj was formally crowned as the Chhatrapati, or emperor of the Maratha Empire, in 1674. The coronation took place at the Raigad fort. Shivaji promoted the use of Marathi and Sanskrit in court and administration, rather than using Persian which was the norm in those times." }
 ];
 
 export default GalleryData;
