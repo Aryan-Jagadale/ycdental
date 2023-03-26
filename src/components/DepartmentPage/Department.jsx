3d16fcc7f5381f20bf0cdd1b1d59f70b9d77c1b1
Pallavi Langhe <1032202021@mitwpu.edu.in>
2023-03-26 16:12:05 +0530

    update: welcome pulication list nd academic

--- src/components/DepartmentPage/Department.jsx
+++ src/components/DepartmentPage/Department.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import "../../style/DepartmentsPage/department.css";
 import { departCompImageLabel } from "../../utils/deptComp";
 import LabelDepartComp from "./Department/LabelDepartComp";
+import PublicationList from "../../assets/pdf/Publication-list.pdf";
+import pdfImg from "../../assets/images/muhs/pdfImg.png";
 
 const Department = () => {
   return (
@@ -12,6 +14,19 @@ const Department = () => {
           <LabelDepartComp item={item} key={item.name} />
         ))}
       </section>
+      <div className="m-5 border-2 border-black p-4">
+        <section className="font-fam flex items-center justify-center gap-3 flex-col">
+          <h2 className="text-3xl tracking-wide font-bold ">Publication</h2>
+          <button className="bg-[#FFD900]  flex items-center justify-center p-2 md:p-3">
+            <img src={pdfImg} alt="PDF" className="mr-2" />
+            <span className="tracking-wide text-blue-500">
+              <a href={PublicationList} target="_blank" rel="noreferrer">
+                Download PDF
+              </a>
+            </span>
+          </button>
+        </section>
+      </div>
     </div>
   );
 };
