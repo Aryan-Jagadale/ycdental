9da3e8bedad0e25ee040326b7e234fa4d15f7658
Pallavi Langhe <1032202021@mitwpu.edu.in>
2023-03-28 00:09:09 +0530

    update: dept

--- src/components/DepartmentPage/Department/LabelDepartComp.jsx
+++ src/components/DepartmentPage/Department/LabelDepartComp.jsx
@@ -7,12 +7,11 @@ const LabelDepartComp = ({ item }) => {
     <div className=" widthAdjust border-b-8 border-[#ECDADA] rounded-lg">
       <div className="border-l-8 border-r-8 border-[#ECDADA] -z-10 rounded-lg">
         <section
-          className={`border-t-8 ${
-            item.yellowColor ? "border-[#F7DC70]" : "border-[#777D9A]"
-          }  rounded-lg bg-white flex flex-col justify-center gap-10 items-center md:w-[20vw] p-3`}
+          className={`border-t-8 ${item.yellowColor ? "border-[#F7DC70]" : "border-[#777D9A]"
+            }  rounded-lg bg-white flex flex-col justify-center gap-10 items-center md:w-[20vw] p-3 `}
         >
           <div>
-            <LazyLoadImage effect="opacity" src={item.label} alt={item.name} />
+            <LazyLoadImage effect="opacity" className="w-20 h-20 " src={item.label} alt={item.name} />
           </div>
           <div>
             <Link to={item.linkUrl}>
