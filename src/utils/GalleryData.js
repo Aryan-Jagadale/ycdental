85b016734e8e8a922a0c4a25498600263560fe91
Pranav shelke <88943865+PranavShelke4@users.noreply.github.com>
2023-03-27 14:02:59 +0000

    .

--- src/utils/GalleryData.js
+++ src/utils/GalleryData.js
@@ -1,12 +1,38 @@
+import DentistDayimg1 from "../assets/images/EventsImg/2.jpg";
+import DentistDayimg2 from "../assets/images/EventsImg/3.jpg";
+import DentistDayimg3 from "../assets/images/EventsImg/4.jpg";
+import DentistDayimg4 from "../assets/images/EventsImg/8.jpg";
+import DentistDayimg5 from "../assets/images/EventsImg/9.jpg";
+
+import AmbedkarJayantiImg1 from "../assets/images/EventsImg/20.jpg";
+import AmbedkarJayantiImg2 from "../assets/images/EventsImg/10.jpg";
+import AmbedkarJayantiImg3 from "../assets/images/EventsImg/11.jpg";
+
+import RajeevChitgoppiImg1 from "../assets/images/EventsImg/15.jpg";
+import RajeevChitgoppiImg2 from "../assets/images/EventsImg/17.jpg";
+import RajeevChitgoppiImg3 from "../assets/images/EventsImg/24.jpg";
+
+import InternationalWomenDayImg1 from "../assets/images/EventsImg/1.jpg";
+import InternationalWomenDayImg2 from "../assets/images/EventsImg/6.jpg";
+import InternationalWomenDayImg3 from "../assets/images/EventsImg/50.jpg";
+
+import ShivJayantiImg1 from "../assets/images/EventsImg/52.jpg";
+import ShivJayantiImg2 from "../assets/images/EventsImg/51.jpg";
+import ShivJayantiImg3 from "../assets/images/EventsImg/56.jpg";
+import ShivJayantiImg4 from "../assets/images/EventsImg/28.jpg";
+import ShivJayantiImg5 from "../assets/images/EventsImg/41.jpg";
+
+
 const GalleryData = [
   {
     id: 1,
     days: "Dentist Day",
     image: [
-        "https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8&w=1000&q=80",
-        "https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8&w=1000&q=80",
-        "https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8&w=1000&q=80",
-        "https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8&w=1000&q=80",
+      DentistDayimg1,
+      DentistDayimg2,
+      DentistDayimg3,
+      DentistDayimg4,
+      DentistDayimg5
     ],
     date: "6th",
     month: "March",
@@ -16,30 +42,59 @@ const GalleryData = [
     },
   {
     id: 2,
-    /*days: "Dentist Day",*/
+    days: "Ambedkar Jayanti",
     image: [
-        "https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8&w=1000&q=80",
-        "https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8&w=1000&q=80",
+      AmbedkarJayantiImg1,
+      AmbedkarJayantiImg2,
+      AmbedkarJayantiImg3,
     ],
-    date: "6th",
-    month: "March",
+    date: "14th",
+    month: "April",
     description:
-      "Every year March 6th is observed as National Dentist Day, created to say thank you to all dentists celebrating for their contribution.There are various dental specialities and on this special occasion, it is better to know and recognise them. Dentists are often referred to as dental surgeons or family dentist. They are specialised in treating oral health; it is a comprehensive integrated approach in a field to overall health.",
-  
+    "The entire nation knows about the legendary “Bhimrao Ramji Ambedkar”. He was one of the greatest leaders of India who was more of a social worker, a writer, and an educationist. He always worked for the Social Justice of untouchables and was the one who drafted Indian Constitution. He was like an angel for the poor and low caste people, this is a reason people still worship him like a God."
     },
   {
     id: 3,
-    days: "Dentist Day",
+    days: "What After BDS - Rajeev Chitgoppi",
     image: [
-        "https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8&w=1000&q=80",
-        "https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8&w=1000&q=80",
+      RajeevChitgoppiImg1,
+      RajeevChitgoppiImg2,
+      RajeevChitgoppiImg3,
     ],
     date: "6th",
     month: "March",
     description:
     "Every year March 6th is observed as National Dentist Day, created to say thank you to all dentists celebrating for their contribution.There are various dental specialities and on this special occasion, it is better to know and recognise them. Dentists are often referred to as dental surgeons or family dentist. They are specialised in treating oral health; it is a comprehensive integrated approach in a field to overall health.",
-    
   },
+  {
+    id: 4,
+    days: "International Women Day",
+    image: [
+      InternationalWomenDayImg1,
+
+      InternationalWomenDayImg3,
+      InternationalWomenDayImg2,  
+
+    ],
+    date: "8th",
+    month: "March",
+    description:
+      "An equal world is an enabled world. How will you help forge a gender equal world? Celebrate women's achievement. Raise awareness against bias. Take action for equality.International Women's Day (IWD) is celebrated on the 8th of March every year around the world. It is a focal point in the movement for women's rights." },
+  {
+    id: 5,
+    days: "Shiv Jayanti",
+    image: [
+      ShivJayantiImg1,
+      ShivJayantiImg2,
+      ShivJayantiImg3,
+      ShivJayantiImg4,
+      ShivJayantiImg5,
+
+      ],
+    date: "19th",
+    month: "February",
+    description:
+     "Shiv Jayanti or Chhatrapati Shivaji Maharaj Jayanti is the birth anniversary of Maratha king Shivaji. Shiv Jayanti is celebrated on February 19 every year in his honour.The creator and founder of the Maratha Empire, Chhatrapati Shivaji Maharaj was born in 1630 at Shivneri Fort near Pune.He was named after a regional Goddess Shivai. Chhatrapati Shivaji Maharaj was formally crowned as the Chhatrapati, or emperor of the Maratha Empire, in 1674. The coronation took place at the Raigad fort. Shivaji promoted the use of Marathi and Sanskrit in court and administration, rather than using Persian which was the norm in those times." }
 ];
 
 export default GalleryData;
