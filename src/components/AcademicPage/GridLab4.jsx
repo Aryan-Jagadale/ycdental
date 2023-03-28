107cb574fb6d1266485d7ce1d8c80403b9c6848a
Pranav shelke <88943865+PranavShelke4@users.noreply.github.com>
2023-03-28 07:05:35 +0000

    done

--- src/components/AcademicPage/GridLab4.jsx
+++ src/components/AcademicPage/GridLab4.jsx
@@ -18,15 +18,19 @@ export const GridLab4 = ({ item }) => {
 export const GridLab3 = ({ item }) => {
   return (
     <>
-      <div className="h-[25vh] text-center md:h-[35vh] w-[80%] md:w-[50vw] border relative  border-gray-500 shadow-xl hover:shadow-2xl rounded-xl divSpan ">
+      <div id="container" className="h-[25vh] text-center md:h-[35vh] w-[80%] md:w-[50vw] border relative  border-gray-500 shadow-xl hover:shadow-2xl rounded-xl divSpan ">
         <LazyLoadImage
           src={item.imG}
           alt={item.label}
           effect={"opacity"}
-          className="h-full w-full object-cover rounded-lg transform transition duration-500 hover:scale-105"
+          className="h-full w-full object-cover rounded-lg"
+          id="galleryImges"
         />
-        <div>
+        {/* <div>
           <h4 className="font-fam font-bold tracking-wide">{item.label}</h4>
+        </div> */}
+        <div className="middle">
+          <h4 className="text font-fam font-bold tracking-wide">{item.label}</h4>
         </div>
       </div>
     </>
