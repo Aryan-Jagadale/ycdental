f8591d98de74ee930ad787e85fe03fa026c0f4e3
Pallavi Langhe <1032202021@mitwpu.edu.in>
2023-03-28 22:31:33 +0530

    added committees json file

---
+++ src/utils/Committees.js
@@ -0,0 +1,227 @@
+export const antiCommittee = [
+  {
+    name: "Dr.Shripad Rajhans",
+    designation: "Chairman",
+    num: 7720013322,
+  },
+  {
+    name: "Dr.Nilima Rajhans",
+    designation: "Secretary Member",
+    num: 7720013317,
+  },
+  {
+    name: "Dr.Dinesh Rajput",
+    designation: "Member",
+  },
+  {
+    name: "Dr.Anuradha Aradhye",
+    designation: "Member",
+  },
+  {
+    name: "Dr.Nikil Bombale",
+    designation: "Member",
+  },
+  {
+    name: "Dr.Supriya Vyavahare",
+    designation: "Member",
+  },
+  {
+    name: "Dr.Rahul Anand",
+    designation: "Member",
+  },
+  {
+    name: "Dr.Shailee Shelke",
+    designation: "Member",
+  },
+  {
+    name: "Nidhi Gujar",
+    designation: "Student Member",
+  },
+  {
+    name: "Sunil Ghante",
+    designation: "Student Member",
+  },
+  {
+    name: "Nidhi Jondhale",
+    designation: "Student Member",
+  },
+];
+
+export const grievanceCommittee = [
+  {
+    name: "Dr.Nilima Rajhans",
+    designation: "Chairman",
+  },
+  {
+    name: "Dr.Anuradha Aradhye",
+    designation: "Incharge",
+  },
+  {
+    name: "Dr.Dinesh Rajput",
+    designation: "Member",
+  },
+  {
+    name: "Dr.Nikil Bombale",
+    designation: "Member",
+  },
+  {
+    name: "Dr.Khanzode Suchet",
+    designation: "Member",
+  },
+];
+
+export const libraryCommittee = [
+  {
+    name: "Dr.Nilima Rajhans",
+    designation: "Chairman",
+  },
+  {
+    name: "Shri.Kiran Darandale",
+    designation: "Incharge",
+  },
+  {
+    name: "Dr.Omkar Sapkal",
+    designation: "Member",
+  },
+  {
+    name: "Dr.Pratik Attal",
+    designation: "Member",
+  },
+  {
+    name: "Dr.Vasim Mubashiruddin",
+    designation: "Member",
+  },
+];
+
+export const parentTeacherCommittee = [
+  {
+    name: "Dr.Nilima Rajhans",
+    designation: "Chairman",
+  },
+  {
+    name: "Dr.Nilima Daule",
+    designation: "Incharge",
+  },
+  {
+    name: "Dr.Snehal Palve",
+    designation: "Member",
+  },
+  {
+    name: "Dr.Bhawana Gaikwad",
+    designation: "Member",
+  },
+  {
+    name: "Dr.Dinesh Rajput",
+    designation: "Member",
+  },
+];
+
+export const vishakhaCommittee = [
+  {
+    name: "Dr.Nilima Rajhans",
+    designation: "Chairman / Presiding officer",
+  },
+  {
+    name: "Mrs. Jyoti Ranjit Jadhav",
+    designation: "NGO Member",
+  },
+  {
+    name: "Dr.Aradhye Anuradha",
+    designation: "Member",
+  },
+  {
+    name: "Dr.V.M.V.Madhav",
+    designation: "Member",
+  },
+  {
+    name: "Dr.Manisha Kulkarni",
+    designation: "Member",
+  },
+  {
+    name: "Dr.Sushma Bommanvar",
+    designation: "Member",
+  },
+  {
+    name: "Dr.Rekha Jarad",
+    designation: "Hostel rector",
+  },
+];
+
+export const alumniCommittee = [
+  {
+    name: "Dr.Nilima Rajhans",
+    designation: "Chairman",
+  },
+  {
+    name: "Dr.Aradhye Anuradha",
+    designation: "Teacher Incharge",
+  },
+  {
+    name: "Dr.Snehal Palwe",
+    designation: "Student Incharge",
+  },
+  {
+    name: "Dr.Pratil Atal",
+    designation: "Member",
+  },
+  {
+    name: "Dr.Tejas Patil",
+    designation: "Member",
+  },
+  {
+    name: "Dr.Kavita Shirsath",
+    designation: "Member",
+  },
+  {
+    name: "Dr.Komal Athare",
+    designation: "Member",
+  },
+  {
+    name: "Dr.Sachin Sake",
+    designation: "Member",
+  },
+  {
+    name: "Dr.Nitish Ingle",
+    designation: "Member",
+  },
+];
+
+export const studentCouncilCommittee = [
+  {
+    name: "Dr.Nilima Rajhans",
+    designation: "Chairman",
+  },
+  {
+    name: "Dr Manisha Kulkarni",
+    designation: "Member",
+  },
+  {
+    name: "Dr.Aradhye Anuradha",
+    designation: "Member",
+  },
+
+  {
+    name: "Dr Dhanesh Sable",
+    designation: "Member",
+  },
+  {
+    name: "Dr Nikhil Bomble",
+    designation: "Member",
+  },
+  {
+    name: "Mahek Sewlani - 1st year",
+    designation: "Student Member",
+  },
+  {
+    name: "Rasika Bansode - 2nd year",
+    designation: "Student Member",
+  },
+  {
+    name: "Rishika Sharma - 3rd year",
+    designation: "Student Member",
+  },
+  {
+    name: "Diksha Asrani - 4th year",
+    designation: "Student Secretary",
+  },
+];
