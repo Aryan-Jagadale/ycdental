32a9fed3d40fcdd44af985d666bb6015d9399e1f
Aryan-Jagadale <aryansjagadale@gmail.com>
2023-03-29 20:12:55 +0530

    All done

--- src/components/HomePage/Carosuel.jsx
+++ src/components/HomePage/Carosuel.jsx
@@ -1,31 +1,23 @@
 import React from "react";
 import { Carousel } from "flowbite-react";
-import InnerCaroComp from "./InnerCaroComp";
+import {InnerCaroComp,InnerCaroComp1 } from "./InnerCaroComp";
 import "../../style/HomePage/carosuel.css";
-import { carousel } from "../../utils/Carousel";
+import { carousel, carousel1 } from "../../utils/Carousel";
 
 const CarosuelComp = () => {
   return (
     <>
       <div className="carosuel">
         <Carousel className="carosuel" slide={false}>
-          {carousel.map((item,index) => (
-            
-
-            <InnerCaroComp
-            left={item.left}
-            right={item.right}
-            
-            
-            />
-         
-              
-
-            
-
-           
-
+          {carousel.map((item) => (
+            <InnerCaroComp left={item.left} right={item.right} />
           ))}
+          {
+            carousel1.map((item)=> (
+              <InnerCaroComp1 backImg={item.backImg} key={item.backImg} />
+            ))
+          }
+
         </Carousel>
       </div>
     </>
