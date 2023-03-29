f8473e24e113b33557f5397b8fbfc99d5d12c01c
Aryan-Jagadale <aryansjagadale@gmail.com>
2023-03-29 20:12:55 +0530

    All done

--- src/components/HomePage/InnerCaroComp.jsx
+++ src/components/HomePage/InnerCaroComp.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 
-const InnerCaroComp = ({ left, right,backImg }) => {
+export const InnerCaroComp = ({ left, right }) => {
   return (
-    <>
-      <div className="carosuel-bg" style={{
-        backgroundImage:backImg
-      }}>
+    
+      <div className="carosuel-bg bg-[url('https://res.cloudinary.com/ddaalka6i/image/upload/v1679145925/buil_1_afzg2u.jpg')]">
         <div className="h-[100%] w-[100%] md:h-[80%] md:w-2/3 absolute top-1/2 left-1/2 -translate-x-2/4 -translate-y-1/2 border-4 border-[#800000] backdrop-blur-sm">
           <section className="p-4 h-full flex items-center justify-between relative">
             <div className="h-full w-full md:p-4">
@@ -24,7 +22,7 @@ const InnerCaroComp = ({ left, right,backImg }) => {
               />
             </div>
           </section>
-          {/*Strip */}
+          
           <section
             className="bg-[#800000] h-[20%] w-full absolute top-1/2 left-1/2 -translate-x-2/4 -translate-y-1/2 flex items-center justify-center"
             style={{
@@ -36,12 +34,23 @@ const InnerCaroComp = ({ left, right,backImg }) => {
               More than Dentistry
             </span>
           </section>
-        </div>
+          </div>
+      </div>
+    
+
+  
+  );
+};
+
+export const InnerCaroComp1 = ({backImg }) => {
+  return (
+    <>
+      <div className="carosuel-bg">
+      <img src={backImg} className="h-full w-full object-cover" alt="Banner" />
+        
       </div>
     </>
 
   
   );
 };
-
-export default InnerCaroComp;
--- src/utils/Carousel.js
+++ src/utils/Carousel.js
@@ -1,44 +1,58 @@
 import Left1 from "../assets/images/Home/Left1.jpeg";
 import Right1 from "../assets/images/Home/Right1.jpg";
 
+import Sec2 from "../assets/images/Home/Carousel/2.jpg";
+import Sec3 from "../assets/images/Home/Carousel/3.jpg";
+import Sec4 from "../assets/images/Home/Carousel/4.jpg";
+import Sec5 from "../assets/images/Home/Carousel/5.jpg";
+import Sec6 from "../assets/images/Home/Carousel/6.jpeg";
+//import Sec2 from "../assets/images/Home/Carousel/2.jpg";
 
 export const carousel = [
-    {
-        /*backImg:"https://res.cloudinary.com/ddaalka6i/image/upload/v1679145925/buil_1_afzg2u.jpg",*/
-        left:Left1,
-        //midd:"https://res.cloudinary.com/ddaalka6i/image/upload/v1679144317/1_ejjj8b.jpg",
-        right:Right1
-    },
-    {
-        left:Left1,
-        //midd:"https://res.cloudinary.com/ddaalka6i/image/upload/v1679144317/1_ejjj8b.jpg",
-        right:Right1
+  {
+    backImg:
+      "https://res.cloudinary.com/ddaalka6i/image/upload/v1679145925/buil_1_afzg2u.jpg",
+    left: Left1,
+    //midd:"https://res.cloudinary.com/ddaalka6i/image/upload/v1679144317/1_ejjj8b.jpg",
+    right: Right1,
+  },
+];
 
-    }
-   
-]
+export const carousel1 = [
+  {
+    backImg: Sec2,
+  },
+  {
+    backImg: Sec3,
+  },
+  {
+    backImg: Sec4,
+  },
+  {
+    backImg: Sec5,
+  },
+  {
+    backImg: Sec6,
+  },
+];
 
 export const aboutCarousel = [
-// {
-//     "carImg":"https://res.cloudinary.com/ddaalka6i/image/upload/v1679201869/41_acpdu7.jpg"
-// },
-// {
-//     "carImg":"https://res.cloudinary.com/ddaalka6i/image/upload/v1679201869/7_qw9fhd.jpg"
-// },
-// {
-//     "carImg":"https://res.cloudinary.com/ddaalka6i/image/upload/v1679201870/34_wyetos.jpg"
-// },
-// {
-//     "carImg":"https://res.cloudinary.com/ddaalka6i/image/upload/v1679201870/44_tdulbv.jpg"
-// },
-// {
-//     "carImg":"https://res.cloudinary.com/ddaalka6i/image/upload/v1679201871/21_vvkod9.jpg"
-// },
-// {
-//     "carImg":"https://res.cloudinary.com/ddaalka6i/image/upload/v1679201874/27_wogv3u.jpg"
-// },
-
-
-
-
- ]
+  // {
+  //     "carImg":"https://res.cloudinary.com/ddaalka6i/image/upload/v1679201869/41_acpdu7.jpg"
+  // },
+  // {
+  //     "carImg":"https://res.cloudinary.com/ddaalka6i/image/upload/v1679201869/7_qw9fhd.jpg"
+  // },
+  // {
+  //     "carImg":"https://res.cloudinary.com/ddaalka6i/image/upload/v1679201870/34_wyetos.jpg"
+  // },
+  // {
+  //     "carImg":"https://res.cloudinary.com/ddaalka6i/image/upload/v1679201870/44_tdulbv.jpg"
+  // },
+  // {
+  //     "carImg":"https://res.cloudinary.com/ddaalka6i/image/upload/v1679201871/21_vvkod9.jpg"
+  // },
+  // {
+  //     "carImg":"https://res.cloudinary.com/ddaalka6i/image/upload/v1679201874/27_wogv3u.jpg"
+  // },
+];
