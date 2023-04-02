3d5c110da1d5113060d2ef57b2b265b420137984
Aryan-Jagadale <aryansjagadale@gmail.com>
2023-04-02 14:41:07 +0530

    Done all small changes

--- src/components/AboutPage/AboutCarousel.jsx
+++ src/components/AboutPage/AboutCarousel.jsx
@@ -1,44 +1,8 @@
 import React from "react";
-import Slider from "react-slick";
 import "../../style/AboutPage/aboutCarousel.css";
-import { aboutCarousel } from "../../utils/Carousel";
-import AboutInnerCarosel from "./AboutInnerCarosel";
 
 const AboutCarousel = () => {
-  const settings = {
-    focusOnSelect: true,
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    speed: 500,
-    dots: true,
-    responsive: [
-      {
-        breakpoint: 1193,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
-  };
+  
 
   return (
     <div style={{height: "100%", marginBottom: "-1.5rem"}}>
@@ -56,17 +20,7 @@ const AboutCarousel = () => {
             </div>
             <br />
 
-            <div className="slider_wrapper px-10 mt-[5vh] lg:mt-[11vh]">
-              <Slider {...settings}>
-                {
-                  aboutCarousel.map((item) => (
-                    <AboutInnerCarosel carImg={item.carImg} />
-
-                  ))
-                }
-
-              </Slider>
-            </div>
+            
             <br />
           </div>
         </div>
--- src/components/HomePage/InnerCaroComp.jsx
+++ src/components/HomePage/InnerCaroComp.jsx
@@ -1,59 +1,19 @@
 import React from "react";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 
-export const InnerCaroComp = ({ left, right }) => {
-  return (
-    
-      <div className="carosuel-bg bg-[url('https://res.cloudinary.com/ddaalka6i/image/upload/v1679145925/buil_1_afzg2u.jpg')]">
-        <div className="h-[100%] w-[100%] md:h-[80%] md:w-2/3 absolute top-1/2 left-1/2 -translate-x-2/4 -translate-y-1/2 border-4 border-[#800000] backdrop-blur-sm">
-          <section className="p-4 h-full flex items-center justify-between relative">
-            <div className="h-full w-full md:p-4">
-              <LazyLoadImage
-                className="h-full w-full collageImg1"
-                src={left}
-                alt="dentist1"
-                effect="opacity"
-              />
-            </div>
-           
-            <div className="h-full w-full md:p-4">
-              <LazyLoadImage
-                className="h-full w-full collageImg3"
-                src={right}
-                alt="dentist2"
-                effect="opacity"
-              />
-            </div>
-          </section>
-          
-          <section
-            className="bg-[#800000] h-[20%] w-full absolute top-1/2 left-1/2 -translate-x-2/4 -translate-y-1/2 flex items-center justify-center"
-            style={{
-              background: "rgba(128, 0, 0, 0.68)",
-              backdropFilter: "blur(5px)",
-            }}
-          >
-            <span className="text-[#FFD900] font-bold font-2xl font-fam uppercase tracking-wide">
-              More than Dentistry
-            </span>
-          </section>
-          </div>
-      </div>
-    
 
-  
-  );
-};
 
-export const InnerCaroComp1 = ({backImg }) => {
+export const InnerCaroComp1 = ({ backImg }) => {
   return (
     <>
-      <div className="carosuel-bg">
-      <LazyLoadImage src={backImg} className="h-full w-full object-cover" alt="Banner" effect="opacity" />
-        
+      <div>
+        <LazyLoadImage
+          src={backImg}
+          className="h-[90vh] w-full carosuel-bg"
+          alt="Banner"
+          effect="opacity"
+        />
       </div>
     </>
-
-  
   );
 };
