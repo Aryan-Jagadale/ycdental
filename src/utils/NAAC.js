7f774bebbd0d88666317bad368462d3bc247456f
Pallavi Langhe <1032202021@mitwpu.edu.in>
2023-04-11 14:48:43 +0530

    update mhus

---
+++ src/utils/NAAC.js
@@ -0,0 +1,53 @@
+export const pdfRow = [
+    {
+      label: "NAAC Achievement" ,
+      url: "",
+    },
+    {
+      label: "NAAC Achievement ",
+      url: "",
+    },
+    {
+      label: "NAAC Achievement",
+      url: "",
+    },
+    {
+        label: "NAAC Achievement" ,
+        url: "",
+      },
+      {
+        label: "NAAC Achievement ",
+        url: "",
+      },
+      {
+        label: "NAAC Achievement",
+        url: "",
+      },
+  ];
+  
+  export const pdfRow1 = [
+    {
+      label: "NAAC Achievement ",
+      url: "",
+    },
+    {
+      label: "NAAC Achievement ",
+      url: "",
+    },
+    {
+      label: "NAAC Achievement ",
+      url: "",
+    },
+    {
+        label: "NAAC Achievement" ,
+        url: "",
+      },
+      {
+        label: "NAAC Achievement ",
+        url: "",
+      },
+      {
+        label: "NAAC Achievement",
+        url: "",
+      },
+  ];
