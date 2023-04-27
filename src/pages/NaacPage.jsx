bfb14623d318e4989847fee5c2b58e7fde4de9d1
Aryan-Jagadale <aryansjagadale@gmail.com>
2023-04-27 17:52:28 +0530

    done

--- src/pages/NaacPage.jsx
+++ src/pages/NaacPage.jsx
@@ -25,8 +25,8 @@ function NaccPage() {
         </>
       ) : (
         <form onSubmit={checkPw}>
-        <div className="relative LockSection mb-3" data-te-input-wrapper-init>
-          <h1 className="mb-4">Enter Password</h1>
+        <div className="relative LockSection" data-te-input-wrapper-init>
+          <h1 className="mb-4 text-lg">Enter Password</h1>
           <input
             type="text"
             className="peer block min-h-[auto] w-full rounded bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
