b7f85eb6d362efac717f80a3ac47ed73149c4501
Pranav shelke <88943865+PranavShelke4@users.noreply.github.com>
2023-04-28 13:39:15 +0000

    Academics done

--- src/content/Academics/courseOutcomes.js
+++ src/content/Academics/courseOutcomes.js
@@ -1,6 +1,6 @@
 export const courseProgramOutcomes = [
     {
         "title":"Course & Program Outcomes",
-        "pdfLink":'https://res.cloudinary.com/dbdni9n4i/image/upload/v1681289431/YCDENTAL/pdf/Academic/CourseOutcomes1_pvz1h6.pdf'
+        "pdfLink":'https://files.yashdental.org/wp-content/uploads/2023/04/CourseOutcomes1.pdf'
     }
 ]
--- src/content/Academics/publication.js
+++ src/content/Academics/publication.js
@@ -1,6 +1,6 @@
 export const publication = [
     {
         "title":"Publications",
-        "pdfLink":'https://res.cloudinary.com/dbdni9n4i/image/upload/v1681131837/YCDENTAL/pdf/Publication/Publication-list_frxddo.pdf'
+        "pdfLink":'https://files.yashdental.org/wp-content/uploads/2023/04/Publication-list.pdf'
     }
 ]
