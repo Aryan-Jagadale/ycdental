603537721246eadd7d471fce6a6df0e7a9109347
Pallavi Langhe <1032202021@mitwpu.edu.in>
2023-05-03 19:33:48 +0530

    naac criterion

--- src/components/NAAC/NaacSSR.jsx
+++ src/components/NAAC/NaacSSR.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import "../../style/NAAC/naac.css"
-import pdf from "../../assets/pdf/Naac/6.IQAC COMMITTEE.pdf"
+
 
 function NaacSSR() {
     return (
@@ -11,30 +11,30 @@ function NaacSSR() {
             </h1>
             <div className='app-body-sub flex flex-row md:flex-col'>
                 <div className='flex flex-col w-full md:flex-col'>
-                    <a href={pdf} className='flex-col bg-[#D9D9D9] border-l-8 border-r-8 border-[#00205B] h-12 my-3 px-4 text-center'>
+                    <a href={"/nacc/Criterion1"} className='flex-col bg-[#D9D9D9] border-l-8 border-r-8 border-[#00205B] h-12 my-3 px-4 text-center'>
                         <p className='box-one font-fam text-lg py-2.5 whitespace-normal'>Criterion I </p>
                     </a>
-                    <a href={pdf} className='flex-col bg-[#D9D9D9] border-l-8 border-r-8 border-[#00205B] h-12 my-3 px-4 text-center'>
+                    <a href={"/nacc/Criterion2"} className='flex-col bg-[#D9D9D9] border-l-8 border-r-8 border-[#00205B] h-12 my-3 px-4 text-center'>
                         <p className='font-fam text-lg py-2.5 whitespace-normal'>Criterion II </p>
                     </a>
-                    <a href={pdf} className='flex-col bg-[#D9D9D9] border-l-8 border-r-8 border-[#00205B] h-12 my-3 px-4 text-center'>
+                    <a href={"/nacc/Criterion3"} className='flex-col bg-[#D9D9D9] border-l-8 border-r-8 border-[#00205B] h-12 my-3 px-4 text-center'>
                         <p className='font-fam text-lg py-2.5 whitespace-normal'>Criterion III </p>
                     </a>
-                    <a href={pdf} className='flex-col bg-[#D9D9D9] border-l-8 border-r-8 border-[#00205B] h-12 my-3 px-4 text-center'>
+                    <a href={"/nacc/Criterion4"} className='flex-col bg-[#D9D9D9] border-l-8 border-r-8 border-[#00205B] h-12 my-3 px-4 text-center'>
                         <p className='font-fam text-lg py-2.5 whitespace-normal'>Criterion IV </p>
                     </a>
                 </div>
                 <div className='flex flex-col w-full md:flex-col'>
-                    <a href={pdf} className='flex-col bg-[#D9D9D9] border-l-8 border-r-8 border-[#00205B] h-12 mx-2 my-3 px-4 text-center'>
+                    <a href={"/nacc/Criterion5"} className='flex-col bg-[#D9D9D9] border-l-8 border-r-8 border-[#00205B] h-12 mx-2 my-3 px-4 text-center'>
                         <p className='font-fam text-lg py-2.5 whitespace-normal'>Criterion V </p>
                     </a>
-                    <a href={pdf} className='flex-col bg-[#D9D9D9] border-l-8 border-r-8 border-[#00205B] h-12 mx-2 my-3 px-4 text-center'>
+                    <a href={"/nacc/Criterion6"} className='flex-col bg-[#D9D9D9] border-l-8 border-r-8 border-[#00205B] h-12 mx-2 my-3 px-4 text-center'>
                         <p className='font-fam text-lg py-2.5 whitespace-normal'>Criterion VI </p>
                     </a>
-                    <a href={pdf} className='flex-col bg-[#D9D9D9] border-l-8 border-r-8 border-[#00205B] h-12 mx-2 my-3 px-4 text-center'>
+                    <a href={"/nacc/Criterion7"} className='flex-col bg-[#D9D9D9] border-l-8 border-r-8 border-[#00205B] h-12 mx-2 my-3 px-4 text-center'>
                         <p className='font-fam text-lg py-2.5 whitespace-normal'>Criterion VII </p>
                     </a>
-                    <a href={pdf} className='flex-col bg-[#D9D9D9] border-l-8 border-r-8 border-[#00205B] h-12 mx-2 my-3 px-4 text-center'>
+                    <a href={"/nacc/Criterion8"} className='flex-col bg-[#D9D9D9] border-l-8 border-r-8 border-[#00205B] h-12 mx-2 my-3 px-4 text-center'>
                         <p className='font-fam text-lg py-2.5 whitespace-normal'>Criterion VIII </p>
                     </a>
                 </div>
